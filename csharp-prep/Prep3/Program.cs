2f3fc844b35d574db4d8f7bf9e0563000862530d
ARoskelley <aroskelley2112@gmail.com>
2025-09-24 10:00:17 -0600

    Prep 3+4
    Coolio

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int guessNumber = 0;
+        int magicNumber = randomGenerator.Next(1, 20);
+        string answer = "";
+
+        Console.WriteLine("Guess a number between 1 and 20");
+
+        while (guessNumber != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            answer = Console.ReadLine();
+            guessNumber = int.Parse(answer);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("Correct! Congratulations");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,77 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int close = 1;
+        string answer;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished");
+
+        while (close != 0)
+        {
+            Console.WriteLine("Enter a number:");
+            answer = Console.ReadLine();
+            close = int.Parse(answer);
+            if (close != 0)
+            {
+                numbers.Add(close);
+            }
+            
+        }
+
+        int sum = Sum(numbers);
+        float ave = Average(numbers);
+        int lar = Largest(numbers);
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+        Console.WriteLine($"The average of the numbers is: {ave}");
+        Console.WriteLine($"The largest number is: {lar}");
+    }
+
+    static int Sum(List<int> numbers)
+    {
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    static float Average(List<int> numbers)
+    {
+        float ave = 0;
+        int index = 0;
+
+        foreach (int number in numbers)
+        {
+            ave += number;
+            index += 1;
+        }
+
+        ave /= index;
+
+        return ave;
+    }
+    
+    static int Largest(List<int> numbers) {
+        int lar = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > lar)
+            {
+                lar = number;
+            }
+        }
+
+        return lar;
     }
 }
