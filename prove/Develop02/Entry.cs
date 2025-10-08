98f3d32b7b7b3a5283c29b6ed143f0518c0ea6fb
ARoskelley <aroskelley2112@gmail.com>
2025-10-08 10:00:36 -0600

    Class stuff
    aaaaaaa

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,15 +1,16 @@
 public class Entry
 {
-    public string _entryDateTime;
-    public string _entryText;
+    public DateTime _entryDateTime;
+    public List<string> _entryText = new List<string>();
     public string _givenPrompt;
 
     //Behaviors
-
     public void Display()
     {
         Console.Write($"{_entryDateTime} -- ");
         Console.WriteLine($"{_givenPrompt}");
+
+        foreach
         Console.WriteLine($"{_entryText}");
     }
     }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,28 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Entry myEntry = new Entry();
-        myEntry._givenPrompt = "What was the best part of your day?";
-        myEntry._entryDateTime = "Feb 5 2004";
-        myEntry._entryText = "Being born";
+        bool run = true;
 
-        Entry anotherEntry = new Entry();
-        myEntry._givenPrompt = "Favorite eideo game?";
-        myEntry._entryDateTime = "Feb 8 2004";
-        myEntry._entryText = "Elden Ring";
+        List<string> prompts = new List<string>();
+        prompts.Add("Who was the most interesting person I interacted with today?");
+        prompts.Add("What was the best part of my day?");
+        prompts.Add("How did I see the hand of the Lord in my life today?");
+        prompts.Add("What was the strongest emotion I felt today?");
+        prompts.Add("If I had one thing I could do over today, what would it be?");
+        prompts.Add("If today were a flavor of gatorade, what would it be?");
+        prompts.Add("Which cryptid do you identify with most today?");
+        prompts.Add("What crime was most tempting today?");
+        prompts.Add("Is hate speech still hate speech if you love saying it?");
+        prompts.Add("Pickles or mayo?");
 
-        Entry lastEntry = new Entry();
-        myEntry._givenPrompt = "Cool Magic Deck";
-        myEntry._entryDateTime = "Sep 11 2001";
-        myEntry._entryText = "Poison Storm";
+        while (run = true)
+        {
+            Entry myEntry = new Entry();
+            Console.WriteLine("Welcome to your Journal. What would you like to do today?");
+            Console.WriteLine("     - Write (press 1)");
+            Console.WriteLine("     - Display (press 2)");
+            Console.WriteLine("     - Save (press 3)");
+            Console.WriteLine("     - Load (press 4)");
+            Console.WriteLine("     - Quit (press 5)");
 
-        Journal myJournal = new Journal();
-        myJournal.AppendEntry(myEntry);
-        myJournal.AppendEntry(anotherEntry);
-        myJournal.AppendEntry(lastEntry);
-        myJournal.Display();
+            string answer = Console.ReadLine();
+            int response = int.Parse(answer);
+
+            if (response == 1)
+            {
+                Random rnd = new Random();
+                
+                string instancePrompt = prompts[rnd.Next(0, 9)];
+
+                myEntry._givenPrompt = instancePrompt;
+                myEntry._entryDateTime = DateTime.Today;
+
+                Console.Write($"{instancePrompt}: ");
+                myEntry._entryText.Add() = Console.ReadLine();
+            }
+        }
+
     }
 }
